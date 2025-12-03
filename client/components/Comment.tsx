87a538ad3670d576429dabc5e28feb020c9db784
Nikola Bodor <Nikola.Bodor@gmail.com>
2025-12-03 14:17:39 +1300

    refactor: indents lined up

--- client/components/Comment.tsx
+++ client/components/Comment.tsx
@@ -22,18 +22,18 @@ export function Comment({ commentData }: { commentData: CommentWithAuthor }) {
 
   return (
     <div className="flex gap-2">
-    <div className="mb-2 flex flex-row gap-4 rounded-lg border bg-white p-2">
-      <div className="flex h-14 max-h-14 w-14 max-w-14 items-center justify-center overflow-hidden rounded-full  p-1 shadow-sm">
-        <button onClick={handleProfileClick}>
-          <Image
-            className="h-14 max-h-14 w-14 max-w-14 rounded-full border-2 border-[#9cc574]"
-            cloudName="dfjgv0mp6"
-            publicId={publicId}
-            alt={commentData.userName + "'s profile"}
-            crop="fill"
-          />
-        </button>
-      </div>
+      <div className="mb-2 flex flex-row gap-4 rounded-lg border bg-white p-2">
+        <div className="flex h-14 max-h-14 w-14 max-w-14 items-center justify-center overflow-hidden rounded-full  p-1 shadow-sm">
+          <button onClick={handleProfileClick}>
+            <Image
+              className="h-14 max-h-14 w-14 max-w-14 rounded-full border-2 border-[#9cc574]"
+              cloudName="dfjgv0mp6"
+              publicId={publicId}
+              alt={commentData.userName + "'s profile"}
+              crop="fill"
+            />
+          </button>
+        </div>
 
         {commentData.message && (
           <p className="flex items-center justify-center">
